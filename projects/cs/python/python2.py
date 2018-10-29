bb63218a484caf4daf03022b5df07d2c6433eb49
grant <soundbreakr1@gmail.com>
2018-10-29 01:25:46 -0700

    10-29-18
    made a commut

---
+++ projects/cs/python/python2.py
@@ -0,0 +1,12 @@
+import random
+for i in range(30):
+    print(i)
+    print('your number for i is ')
+    print(random.randint(0,100))
+
+"""
+this is a comment ment to last until 2090;
+I first imported the module random so that I could use the import functionself.
+next I decided to use the python for loop, i in range, starting at 0 to 30;
+after all that, I printed my random number with the random function.
+"""
---
+++ projects/cs/ruby/ruby1.rb
@@ -0,0 +1,8 @@
+p=3
+p*4
+p
+said = 'I have more fun than you do.'
+said = said.upcase
+puts said
+10.times {puts said.downcase}
+10.times {puts said.upcase}
