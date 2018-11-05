a6dd113ab146e4d0a7f73cb8e33b75304a803d50
grant <soundbreakr1@gmail.com>
2018-11-05 10:32:26 -0800

    commit 11-5-18
    11-5-18

---
+++ cs-160/cs/Perl/z.pl
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+use diagnostics;
+
+print "hello\nworld\n";
+
+my $name = 'Derek';
+my ($age,$street) = (70, 'west hill valuey ');
+my $my_info = qq{$name lives at this address. \"$street\"\n };
+print ($my_info);
