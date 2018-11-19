5b4d9167df706c4ecbde7467d0eff10ad98a900d
grant <soundbreakr1@gmail.com>
2018-11-19 05:27:34 -0800

    swift
    swift

---
+++ cs-160/cs/swift/swift2.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "Grant"
+let greeting = " \("Hi there,") \(name)"
+print(greeting)
