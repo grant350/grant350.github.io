bd91e033e51a4e0953180aebf71082ff8dfc0246
grant <soundbreakr1@gmail.com>
2018-11-19 04:57:39 -0800

    no
    no

--- cs-160/cs/python/python.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-raw_html = open('./index.html').read()
-html = BeautifulSoup(raw_html, 'html.parser')
-for input in html.select('input'):
-     if input['class'] == 'password':
-        print(input.text)
